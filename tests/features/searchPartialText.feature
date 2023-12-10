a1acb3e38ad3ac146b24be84d1c94f68c73d8647
Akshaya <86973154+Akshutechy@users.noreply.github.com>
2023-12-10 16:24:06 +0530

    req1_BDD

--- tests/features/searchPartialText.feature
+++ tests/features/searchPartialText.feature
@@ -2,9 +2,14 @@ Feature: Search Functionality validation
 
   Scenario Outline: To validate if the search results contains searched text only
     Given User is on the oranum home page
-    When User searches the pshycics with <partial> text
-    Then User should be displayed with search results matching the searched text
-    Examples:
-      | partial |
-      | matt    |
-      |akshaya  |
+    When User searches the pshycics with "<word>" text
+    Then User should be displayed with search results matching the searched text "<word>"
+Examples:
+      | word |
+      | Matt |
+      | Myst |
+      | Ann  |
+      | Psy  |
+      | Jas  |
+      | red  |
+      
--- tests/step_definitions/searchPartialText.steps.js
+++ tests/step_definitions/searchPartialText.steps.js
@@ -5,14 +5,17 @@ Given('User is on the oranum home page', () => {
   I.amOnPage('/');
 });
 
-When('User searches the pshycics with <partial> text', () => {
+When('User searches the pshycics with {string} text', (word) => {
   I.seeElement('.toolbar-search-input');
   I.click('.toolbar-search-input');
-  I.fillField('.toolbar-search-input','matt');
+  I.fillField('.toolbar-search-input',word);
   I.pressKey('Enter');
+  
 });
 
-Then('User should be displayed with search results matching the searched text', () => {
-  I.see('MATT','h1');
-  I.see('Matt','.thumb-data-item--name');
+Then('User should be displayed with search results matching the searched text {string}', (word) => {
+ //Validating the header
+  I.see(word.toUpperCase(),'h1');
+//  Validating the results
+I.see(word,'.thumb-data-item--name');
 });
