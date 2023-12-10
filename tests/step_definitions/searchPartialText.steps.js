b43c165de8a572fb2435212833a7841de3229cb7
Vignesh Srinivasa Raghavan <40608099+vignesh1810@users.noreply.github.com>
2023-12-10 12:08:02 +0100

    Formatted

--- tests/step_definitions/searchPartialText.steps.js
+++ tests/step_definitions/searchPartialText.steps.js
@@ -1,5 +1,4 @@
 const { I } = inject();
-// Add in your custom step files
 
 Given('User is on the oranum home page', () => {
   I.amOnPage('/');
@@ -8,14 +7,14 @@ Given('User is on the oranum home page', () => {
 When('User searches the pshycics with {string} text', (word) => {
   I.seeElement('.toolbar-search-input');
   I.click('.toolbar-search-input');
-  I.fillField('.toolbar-search-input',word);
+  I.fillField('.toolbar-search-input', word);
   I.pressKey('Enter');
-  
 });
 
 Then('User should be displayed with search results matching the searched text {string}', (word) => {
- //Validating the header
-  I.see(word.toUpperCase(),'h1');
-//  Validating the results
-I.see(word,'.thumb-data-item--name');
+  //Validating the header
+  I.see(word.toUpperCase(), 'h1');
+
+  //  Validating the results
+  I.see(word, '.thumb-data-item--name');
 });
